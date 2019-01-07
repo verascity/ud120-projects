dc0a765b1f2a55a95290aa765640fec0073d6734
Vera Sticker <svsticker@gmail.com>
2019-01-07 16:50:08 -0500

    import GaussianNB and fit classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -25,7 +26,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+
 
 
 #########################################################
