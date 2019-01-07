c6af2aa5acc23b54cc9ea27cf432b0482be9a32c
Vera Sticker <svsticker@gmail.com>
2019-01-07 16:56:21 -0500

    add timer

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -28,8 +28,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 clf = GaussianNB()
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
 pred = clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
+
 print accuracy_score(labels_test, pred)
 #########################################################
 
