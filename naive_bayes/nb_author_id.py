2183d31d1689637058a8f6ae2e3697fe0fdcb177
Vera Sticker <svsticker@gmail.com>
2019-01-09 17:01:46 -0500

    cosmetic tweak: adjust spacing

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,9 +23,6 @@ from sklearn.metrics import accuracy_score
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 clf = GaussianNB()
 
