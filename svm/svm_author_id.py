6893b300f17727b7696d71cd590c636d502e5755
Vera Sticker <svsticker@gmail.com>
2019-02-23 12:19:12 -0500

    Shrink dataset to 1/100th size

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,6 +21,9 @@ from sklearn.metrics import accuracy_score
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
 clf = SVC(C=1.0, kernel='linear')
 
 t0 = time()
