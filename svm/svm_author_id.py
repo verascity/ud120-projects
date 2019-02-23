2a15bcc67155270025a88dec7db9f0b839b7d54f
Vera Sticker <svsticker@gmail.com>
2019-02-23 12:16:08 -0500

    Initial upload: linear SVM with full dataset

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +21,14 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = SVC(C=1.0, kernel='linear')
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
 
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+print accuracy_score(labels_test, pred)
